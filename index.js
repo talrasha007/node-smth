c0dfb63642b47866524269a5dbb5545970346b13
Tal Rasha <talrasha007@gmail.com>
2013-02-08 00:54:13 +0800

    add mobile nforum parser.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-﻿var nforumApi = require('./lib/nforumApi.js');
+﻿var nforumApi = require('./lib/nforumApi.js'),
+    webApi = require('./lib/webApi.js');
 
 exports.getClient = function (host, base, appkey, type) {
     if (!type) switch (host) {
         case 'api.byr.cn':
         case 'nforum.byr.edu.cn':
             type = 'nforum';
+            break;
+        case 'm.newsmth.net':
+            type = 'web';
+            break;
     }
 
     switch (type) {
         case 'nforum':
             return new nforumApi(host, base, appkey);
-        case '':
-            return;
+        case 'web':
+            return new webApi(host, base);
     }    
 };
